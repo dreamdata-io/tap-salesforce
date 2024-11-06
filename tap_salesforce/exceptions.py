fefe2670910f1ea20cef78a933c9027346458ab5
gugabfigueiredo <gugabfigueiredo@gmail.com>
2024-11-06 09:09:24 +0100

    allow salesforce exceptions to retry on request (#60)

--- tap_salesforce/exceptions.py
+++ tap_salesforce/exceptions.py
@@ -39,6 +39,37 @@ class SalesforceException(Exception):
     def __str__(self) -> str:
         return f'{self.code}: {super().__str__()}'
 
+
+class SalesforceFunctionalityTemporarilyUnavailableException(SalesforceException):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message, "FUNCTIONALITY_TEMPORARILY_UNAVAILABLE")
+
+
+class SalesforceAPIDisabledForOrganizationException(SalesforceException):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message, "API_DISABLED_FOR_ORG")
+
+
+class SalesforceSessionExpiredException(SalesforceException):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message, "SESSION_EXPIRED")
+
+
+class SalesforceUnexpectedException(SalesforceException):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message, "UNEXPECTED")
+
+
+class SalesforceQueryTimeoutException(SalesforceException):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message, "QUERY_TIMEOUT")
+
+
 # build_salesforce_exception transforms a generic Response into a SalesforceException if the
 # response body has a salesforce exception, returns None otherwise
 # salesforce error body looks like:
@@ -71,5 +102,20 @@ def build_salesforce_exception(resp: Response) -> Optional[SalesforceException]:
         return None
 
     code = err_dict.get("errorCode")
+
+    if code == "FUNCTIONALITY_TEMPORARILY_UNAVAILABLE":
+        return SalesforceFunctionalityTemporarilyUnavailableException(msg)
+
+    if code == "API_DISABLED_FOR_ORG":
+        return SalesforceAPIDisabledForOrganizationException(msg)
+
+    if "Session expired or invalid" in msg:
+        return SalesforceSessionExpiredException(msg)
+
+    if "An unexpected error occurred" in msg:
+        return SalesforceUnexpectedException(msg)
+
+    if "Your query request was running for too long" in msg:
+        return SalesforceQueryTimeoutException(msg)
 
     return SalesforceException(msg, code)
